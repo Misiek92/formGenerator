80528e233684c14f48af4e4c6f1584405139849f
Misiek92 <meteorek@gmail.com>
2015-05-08 13:28:56 +0200

    Some possible to fix JSLint and JSHint errors and code format

--- js/formGenerator.js
+++ js/formGenerator.js
@@ -1,7 +1,7 @@
 /*
- * jQuery plugin 
+ * jQuery plugin
  * Copyright (c) 2015 Mateusz Kuźnik
- * 
+ *
  * *************************************************************************
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
  *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
@@ -12,12 +12,12 @@
  *  IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  *  THE SOFTWARE.
  * *************************************************************************
- * 
+ *
  */
 
 (function ($) {
+    "use strict";
     /*
-     * 
      * @param {type} id - value for form id attribute. *REQUIRED*
      * @param {type} input - array of objects. Each object describe one field. Below possible elements for object:
      *  - id: *REQUIRED*. Will be used to create field id attribute
@@ -30,26 +30,28 @@
      *      # password
      *      # postalcode - for Poland
      *      # hidden
-     *      # title - title for sectors
+     *      # title - title for sector
      *  - name: This value will be visible as placeholder,
      *  - description: short description before input,
      *  - regexp: validation based on regular expression,
      *  - class: CSS class,
      *  - length: field length,
-     *  - break: false by default, set true if you want insert break line after field,
+     *  - breakLine: false by default, set true if you want insert breakLine line after field,
      *  - uppercase: false by default, set true if you want to change value to uppercase
      *  - lowercase: false by default, set true if you want to change value to lowercase,
-     *      
-     *      
+     *
+     *
      * *** EXAMPLE OF OBJECT ***
-     * {id: "test", type: "text", name: "Imie", description: "Jakis opis", regexp: "\d", class: "input", length: 10, break: true, uppercase: true},
+     * {id: "test", type: "text", name: "Imie", description: "Jakis opis", regexp: "\d", class: "input", length: 10, breakLine: true, uppercase: true},
      * *** END ***
-     * 
+     *
      * @returns html form with validation
      */
     $.fn.formGenerator = function (id, input) {
 
-        var form = $("<form>").attr({id: id});
+        var form = $("<form>").attr({
+            id: id
+        });
 
         $.each(input, function (i, val) {
 
@@ -57,28 +59,39 @@
                 /*
                  * List of variables
                  */
-                var fieldId = val.id;
-                var fieldType;
-                var fieldPlaceholder = val.name;
-                var fieldDescription = val.description;
-                var fieldLength = val.length;
-                typeof val.type !== "undefined" && val.type !== "number" ? fieldType = val.type : fieldType = "text";
-                var fieldClass = val.class;
+                var fieldId = val.id,
+                    fieldType = "text",
+                    fieldPlaceholder = val.name,
+                    fieldDescription = val.description,
+                    fieldLength = val.length,
+                    fieldClass = val.className,
+                    field,
+                    ctrlDown = false,
+                    ctrlKey = 17,
+                    vKey = 86,
+                    cKey = 67,
+                    label,
+                    //^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$
+                    mailRegExp = /^((\[^<>()\[\]\\.,;:\s@\"\]+(\.[^<>()\[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                    r = val.regexp;
+                if (val.type !== "undefined" && val.type !== "number") {
+                    fieldType = val.type;
+                }
                 //end
 
                 /*
                  * create input field
                  */
                 if (fieldType !== "select") {
-                    var field = $("<input>")
-                            .attr({
-                                id: fieldId,
-                                type: fieldType,
-                                basetype: val.type,
-                                placeholder: fieldPlaceholder,
-                                maxlength: fieldLength
-                            })
-                            .addClass(val.class);
+                    field = $("<input>")
+                        .attr({
+                            id: fieldId,
+                            type: fieldType,
+                            basetype: val.type,
+                            placeholder: fieldPlaceholder,
+                            maxlength: fieldLength
+                        })
+                        .addClass(val.className);
 
                     /*
                      * Number type based on text
@@ -87,16 +100,15 @@
                         /*
                          * Support for ctrl + c & ctrl + v
                          */
-                        var ctrlDown = false;
-                        var ctrlKey = 17, vKey = 86, cKey = 67;
-                        $(document).keydown(function (e)
-                        {
-                            if (e.keyCode === ctrlKey)
+
+                        $(document).keydown(function (e) {
+                            if (e.keyCode === ctrlKey) {
                                 ctrlDown = true;
-                        }).keyup(function (e)
-                        {
-                            if (e.keyCode === ctrlKey)
+                            }
+                        }).keyup(function (e) {
+                            if (e.keyCode === ctrlKey) {
                                 ctrlDown = false;
+                            }
                         });
                         /*
                          * Actually i didn't find better solution for paste feature
@@ -107,9 +119,9 @@
                              * Regexp supports patterns like
                              * 1234 OR 1.2 OR 1,2
                              */
-                            var pattern = /(^\d*\d*$|^\d+(\.|\,)?\d+$)/;
-                            var element = this; // "paste" element
-                            var before = $(element).val();
+                            var pattern = /(^\d*\d*$|^\d+(\.|\,)?\d+$)/,
+                                element = this, // "paste" element
+                                before = $(element).val();
                             setTimeout(function () {
                                 var text = $(element).val();
                                 console.log(text);
@@ -155,8 +167,6 @@
                     /*
                      * Regular Expression validation
                      */
-                    var mailRegExp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-                    var r = val.regexp;
                     if (typeof val.regexp !== "undefined" || val.type === "mail") {
                         field.keyup(function () {
                             if (typeof r !== "undefined" && r !== null) {
@@ -183,12 +193,12 @@
                     }
                 } else { // Create select object
                     if (typeof val.list !== "undefined" && val.list[0]) {
-                        var field = $("<select>")
-                                .attr({
-                                    id: fieldId
-                                })
-                                .addClass(val.class);
-                        ;
+                        field = $("<select>")
+                            .attr({
+                                id: fieldId
+                            })
+                            .addClass(val.className);
+
 
                         $.each(val.list, function (i, value) {
                             var option = $("<option>");
@@ -201,19 +211,19 @@
                 /*
                  * create label if necessarily
                  */
-                var label = $("<label>")
-                        .attr({
-                            id: "label_" + fieldId,
-                            for : fieldId
-                        }).html(fieldDescription);
+                label = $("<label>")
+                    .attr({
+                        id: "label_" + fieldId,
+                        for: fieldId
+                    }).html(fieldDescription);
                 /*
                  * append elements to form
                  */
                 $(form).append(label);
                 $(form).append(field);
-                if (typeof val.break !== "undefined" && val.break === true) // if val.break
+                if (typeof val.breakLine !== "undefined" && val.breakLine === true) // if val.breakLine
                     $(form).append($("<br>"));
-                
+
                 // set default values
                 if (typeof val.value !== "undefined")
                     $(field).val(val.value);
